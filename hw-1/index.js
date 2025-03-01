77f121c42f69af8d1b2f2afddf1d41e24537705b
gudkozavr <hel98@gmx.de>
2025-03-01 21:46:29 +0100

    end hw-1

---
+++ hw-1/index.js
@@ -0,0 +1,18 @@
+// 1. Начало
+// 2. Ввести число A
+// 3. Ввести число B
+// 4. Ввести число C
+// 5. Вычислить сумму: S = A + B + C
+// 6. Вывести S
+// 7. Конец
+
+// function sumThreeNumbers(a, b, c) {
+//     return a + b + c;
+// }
+
+// const num1 = parseFloat(prompt("Введите первое число:"));
+// const num2 = parseFloat(prompt("Введите второе число:"));
+// const num3 = parseFloat(prompt("Введите третье число:"));
+
+// const sum = sumThreeNumbers(num1, num2, num3);
+// console.log("Сумма трех чисел:", sum);
